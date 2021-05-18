c8030b15ac2ec3c5d647d4caf8b9945ba4cffe9f
Kylory <72569852+Kylory@users.noreply.github.com>
2021-05-18 21:40:20 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import _ from 'lodash';
+
+const refs = {
+  input: document.querySelector('#input'),
+};
+// refs.input.addEventListener('input', log);
+
+refs.input.addEventListener(
+  'input',
+  _.debounce(() => {
+    console.log('qwe');
+  }, 500),
+);
+// let inputCountry = refs.input.value;
+
+// _.debounce(() => {log}, 300)
+
+function log() {
+  const inputCountry = refs.input.value;
+  console.log(inputCountry);
+
+  fetch(`https://restcountries.eu/rest/v2/name/${inputCountry}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+      console.log(country.length);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+// document.querySelector('input').addEventListener(
+//   'input',
+//   _.debounce(() => {
+//     log;
+//   }, 300),
+// );
